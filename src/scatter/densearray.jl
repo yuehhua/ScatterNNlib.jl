e917ab11cb9a4728abed3c0e0a2618fcbfcaaf89
Yueh-Hua Tu <a504082002@gmail.com>
2020-10-05 14:49:01 +0800

    Add doc for scatter

--- src/scatter/densearray.jl
+++ src/scatter/densearray.jl
@@ -1,5 +1,85 @@
 ## Scatter operations
 
+"""
+    scatter_add!(ys, us, xs)
+
+Scatter addition operation.
+
+    ys[:, xs[k]...] = ys[:, xs[k]...] .+ us[:, k...]
+
+# Arguments
+- `ys`: the destination for `us` to aggregate to. This argument will be mutated.
+- `us`: the source data for aggregating.
+- `xs`: the mapping for aggregation from source (index) to destination (value).
+The index of `xs` is corresponding to the index of `us` and the value of `xs` is
+corresponding to the index of `ys`. The value of `xs` can be `Int` or `Tuple` type.
+
+The dimension of `us` must equal to one plus dimension of `xs`. `ys`, `us` and `xs`
+must be supported array type and be the same type.`Array`, `StaticArray` and `CuArray`
+are currently supported.
+"""
+function scatter_add! end
+
+"""
+    scatter_sub!(ys, us, xs)
+
+Scatter subtraction operation.
+
+    ys[:, xs[k]...] = ys[:, xs[k]...] .- us[:, k...]
+
+# Arguments
+- `ys`: the destination for `us` to aggregate to. This argument will be mutated.
+- `us`: the source data for aggregating.
+- `xs`: the mapping for aggregation from source (index) to destination (value).
+The index of `xs` is corresponding to the index of `us` and the value of `xs` is
+corresponding to the index of `ys`. The value of `xs` can be `Int` or `Tuple` type.
+
+The dimension of `us` must equal to one plus dimension of `xs`. `ys`, `us` and `xs`
+must be supported array type and be the same type.`Array`, `StaticArray` and `CuArray`
+are currently supported.
+"""
+function scatter_sub! end
+
+"""
+    scatter_mul!(ys, us, xs)
+
+Scatter multiplication operation.
+
+    ys[:, xs[k]...] = ys[:, xs[k]...] .* us[:, k...]
+
+# Arguments
+- `ys`: the destination for `us` to aggregate to. This argument will be mutated.
+- `us`: the source data for aggregating.
+- `xs`: the mapping for aggregation from source (index) to destination (value).
+The index of `xs` is corresponding to the index of `us` and the value of `xs` is
+corresponding to the index of `ys`. The value of `xs` can be `Int` or `Tuple` type.
+
+The dimension of `us` must equal to one plus dimension of `xs`. `ys`, `us` and `xs`
+must be supported array type and be the same type.`Array`, `StaticArray` and `CuArray`
+are currently supported.
+"""
+function scatter_mul! end
+
+"""
+    scatter_div!(ys, us, xs)
+
+Scatter division operation.
+
+    ys[:, xs[k]...] = ys[:, xs[k]...] ./ us[:, k...]
+
+# Arguments
+- `ys`: the destination for `us` to aggregate to. This argument will be mutated.
+- `us`: the source data for aggregating.
+- `xs`: the mapping for aggregation from source (index) to destination (value).
+The index of `xs` is corresponding to the index of `us` and the value of `xs` is
+corresponding to the index of `ys`. The value of `xs` can be `Int` or `Tuple` type.
+
+The dimension of `us` must equal to one plus dimension of `xs`. `ys`, `us` and `xs`
+must be supported array type and be the same type.`Array`, `StaticArray` and `CuArray`
+are currently supported.
+"""
+function scatter_div! end
+
 for op = [:add, :sub, :mul, :div]
     fn = Symbol("scatter_$(op)!")
     @eval function $fn(ys::Array{T}, us::Array{T}, xs::Array{<:IntOrTuple}) where {T<:Real}
@@ -13,6 +93,24 @@ for op = [:add, :sub, :mul, :div]
     end
 end
 
+"""
+    scatter_max!(ys, us, xs)
+
+Scatter maximum operation.
+
+    ys[:, xs[k]...] = max.(ys[:, xs[k]...], us[:, k...])
+
+# Arguments
+- `ys`: the destination for `us` to aggregate to. This argument will be mutated.
+- `us`: the source data for aggregating.
+- `xs`: the mapping for aggregation from source (index) to destination (value).
+The index of `xs` is corresponding to the index of `us` and the value of `xs` is
+corresponding to the index of `ys`. The value of `xs` can be `Int` or `Tuple` type.
+
+The dimension of `us` must equal to one plus dimension of `xs`. `ys`, `us` and `xs`
+must be supported array type and be the same type.`Array`, `StaticArray` and `CuArray`
+are currently supported.
+"""
 function scatter_max!(ys::Array{T}, us::Array{T}, xs::Array{<:IntOrTuple}) where {T<:Real}
     @simd for k = 1:length(xs)
         k = CartesianIndices(xs)[k]
@@ -23,6 +121,24 @@ function scatter_max!(ys::Array{T}, us::Array{T}, xs::Array{<:IntOrTuple}) where
     ys
 end
 
+"""
+    scatter_min!(ys, us, xs)
+
+Scatter minimum operation.
+
+    ys[:, xs[k]...] = min.(ys[:, xs[k]...], us[:, k...])
+
+# Arguments
+- `ys`: the destination for `us` to aggregate to. This argument will be mutated.
+- `us`: the source data for aggregating.
+- `xs`: the mapping for aggregation from source (index) to destination (value).
+The index of `xs` is corresponding to the index of `us` and the value of `xs` is
+corresponding to the index of `ys`. The value of `xs` can be `Int` or `Tuple` type.
+
+The dimension of `us` must equal to one plus dimension of `xs`. `ys`, `us` and `xs`
+must be supported array type and be the same type.`Array`, `StaticArray` and `CuArray`
+are currently supported.
+"""
 function scatter_min!(ys::Array{T}, us::Array{T}, xs::Array{<:IntOrTuple}) where {T<:Real}
     @simd for k = 1:length(xs)
         k = CartesianIndices(xs)[k]
@@ -33,6 +149,24 @@ function scatter_min!(ys::Array{T}, us::Array{T}, xs::Array{<:IntOrTuple}) where
     ys
 end
 
+"""
+    scatter_mean!(ys, us, xs)
+
+Scatter mean operation.
+
+    ys[:, xs[k]...] = mean.(ys[:, xs[k]...], us[:, k...])
+
+# Arguments
+- `ys`: the destination for `us` to aggregate to. This argument will be mutated.
+- `us`: the source data for aggregating.
+- `xs`: the mapping for aggregation from source (index) to destination (value).
+The index of `xs` is corresponding to the index of `us` and the value of `xs` is
+corresponding to the index of `ys`. The value of `xs` can be `Int` or `Tuple` type.
+
+The dimension of `us` must equal to one plus dimension of `xs`. `ys`, `us` and `xs`
+must be supported array type and be the same type.`Array`, `StaticArray` and `CuArray`
+are currently supported.
+"""
 function scatter_mean!(ys::Array{T}, us::Array{T}, xs::Array{<:IntOrTuple}) where {T<:Real}
     Ns = zero(ys)
     ys_ = zero(ys)
